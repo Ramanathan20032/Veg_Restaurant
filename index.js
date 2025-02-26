0bbebdc14d4e22bea13e269ccad21a8aad32900f
Ramanathan20032 <Ramanathan20032@gmail.com>
2025-02-26 15:29:31 +0530

    add : Dynamic Menu & Event Sections

--- index.js
+++ index.js
@@ -209,3 +209,34 @@ document.addEventListener("DOMContentLoaded", function () {
     wrap: true,
   });
 });
+
+
+// !--------------------------------------------------------------------------------
+// Page-Up
+document.addEventListener("DOMContentLoaded", () => {
+  const pageUp = document.querySelector(".page-up");
+
+  if (!pageUp) {
+    console.error("Error: .page-up element not found in the DOM");
+    return;
+  }
+
+  window.addEventListener("scroll", () => {
+  //   console.log("Current Scroll Position (window.scrollY):", window.scrollY); 
+    if (window.scrollY >= 120) {  // Ensure visibility after 120px
+      pageUp.classList.add("show");
+      // console.log("page-up is now visible!"); 
+    } else {
+      pageUp.classList.remove("show");
+      // console.log("page-up is hidden!"); 
+    }
+  });
+
+  pageUp.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});  
